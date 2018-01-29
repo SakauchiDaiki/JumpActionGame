9d0afec56ef0af64c38bef0c31ff8f61e201f724
SakauchiDaiki <daiki.sakauchi@g.softbank.co.jp>
2018-01-29 11:31:43 +0900

    Initial commit

---
+++ core/src/jp/techacademy/daiki/sakauchi/jumpactiongame/ActivityRequestHandler.java
@@ -0,0 +1,9 @@
+package jp.techacademy.daiki.sakauchi.jumpactiongame;
+
+/**
+ * Created by sakauchidaiki on 2018/01/29.
+ */
+
+public interface ActivityRequestHandler {
+    public void showAds(boolean show);
+}
---
+++ core/src/jp/techacademy/daiki/sakauchi/jumpactiongame/Enemy.java
@@ -0,0 +1,9 @@
+package jp.techacademy.daiki.sakauchi.jumpactiongame;
+
+/**
+ * Created by sakauchidaiki on 2018/01/29.
+ */
+
+//public class Enemy extends GameObject {
+
+//}
